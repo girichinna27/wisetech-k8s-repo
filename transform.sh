74aa58cd2475415bd5a8af8a6607c2633edb9641
gopaljayanythi <gopal.jayanti#opsmx.io>
2024-07-02 19:38:40 +0530

    made shell script more friendly

--- transform.sh
+++ transform.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-for dir in $(find mbe -mindepth 2 -maxdepth 2 -type d -name "deployment"); do
+# Check if a folder argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <folder>"
+  exit 1
+fi
+
+# Assign the first argument to a variable
+folder="$1"
+
+# Check if the provided argument is a valid directory
+if [ ! -d "$folder" ]; then
+  echo "Error: $folder is not a valid directory"
+  exit 1
+fi
+
+
+for dir in $(find $folder -mindepth 2 -maxdepth 2 -type d -name "deployment"); do
   parent_dir=$(dirname "$dir")
   parent_dir_name=$(basename "$parent_dir")
   echo "Directory: $dir"
@@ -16,7 +32,7 @@ for envdir in $(find "$parent_dir"/chart -mindepth 1 -maxdepth 1 -type d ); do
   echo environment is $envdir
   echo moving template files
 
-  cp .helmignore.tmpl "$envdir"/
+  cp .helmignore.tmpl "$envdir"/.helmignore
   sed -e "s/PLACEHOLDER/${parent_dir_name}/g" Chart.tmpl >  "$envdir"/Chart.yaml
   mkdir -p "$envdir"/templates
   mv "$envdir"/deployment.yaml "$envdir"/templates
